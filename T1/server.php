d9e45b610c3c3d23aa17a4bf74148f6b467673ed
PetrutaLuminita <drehlutapetruta@gmail.com>
2021-03-29 12:48:56 +0300

    T2 done

--- T1/server.php
+++ T1/server.php
@@ -8,28 +8,27 @@ set_time_limit(0);
 ob_implicit_flush();
 
 // Create socket
-$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket.\n");
 
 echo "Socket created.\n";
 
 // Bind socket to host and port
-$result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
+$result = socket_bind($socket, $host, $port) or die("Could not bind to socket.\n");
 
 echo "Socket bind successfully.\n";
 
 // Start listening - wait for the client to connect
-$result = socket_listen($socket, 10) or die("Could not set up socket listener\n");
+$result = socket_listen($socket, 10) or die("Could not set up socket listener.\n");
 
 echo "Socket listening...\n";
 
-
 while(true) {
     // This socket is responsible for the communication with the client
-    $connection = socket_accept($socket) or die("Could not accept incoming connection\n");
+    $connection = socket_accept($socket) or die("Could not accept incoming connection.\n");
 
     // Display details about client
-    if (socket_getpeername($connection , $address , $port)) {
-        echo "Client $address : $port is now connected.";
+    if (socket_getpeername($connection, $address, $port)) {
+        echo "Client $address: $port is now connected.\n";
     }
 
     // Get the input and manipulate it
@@ -37,10 +36,10 @@ while(true) {
 
     echo "Client typed: $input. \n ";
 
-    $output = strrev($input) . "\n";
+    $output = "Hi! My response is: " . strrev($input) . "\n";
 
     // Sent back the message
-    socket_write($connection, $output, strlen ($output)) or die("Could not write output\n");
+    socket_write($connection, $output, strlen ($output)) or die("Could not send response.\n");
 }
 
 // Close connection
---
+++ T2/productAPI.js
@@ -0,0 +1,299 @@
+const http = require ('http');
+const port = 3300;
+
+let products = [
+	{ id: 1, name: 'Apple', description: 'description apple' },
+	{ id: 2, name: 'Cookie', description: 'description cookie' },
+	{ id: 3, name: 'Water', description: 'description water' },
+	{ id: 4, name: 'Chips', description: 'description chips' },
+	{ id: 5, name: 'Grapefruit', description: 'description grapefruit' },
+	{ id: 6, name: 'Pen', description: 'description pen' },
+];
+
+const requestHandler = (request, response) => {
+	switch(request.method) {
+		case 'HEAD':
+			headHandler(request,response);
+			break;
+
+		case 'GET':
+			getHandler(request,response);
+			break;
+
+		case 'POST':
+			postHandler(request,response);
+			break;
+
+		case 'PUT':
+			putHandler(request,response);
+			break;
+
+		case 'PATCH':
+			patchHandler(request,response);
+			break;
+
+		case 'DELETE':
+			deleteHandler(request,response);
+			break;
+
+		default:
+			fallbackHandler(response);
+			break;
+	}
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, err => {
+	if (err) {
+		return console.log('Connection error', err)
+	}
+	
+    console.log('Server is listening on port ' + port)
+});
+
+
+function headHandler(request,response) {
+	if (request.url === '/products') {
+		response.setHeader('Content-Type', 'application/json');
+		response.statusCode = 200;
+		response.end();
+	} else if (request.url.split('/')[1] == 'products' && request.url.split('/')[2]) {
+		const product = products.find((f) => f.id == request.url.split('/')[2]);
+
+		if (product) {
+			response.setHeader('Content-Type', 'application/json');
+			response.statusCode = 200;
+			response.end();
+		} else {
+			response.statusCode = 404;
+			response.end('Product not found!')
+		}
+	} else {
+		response.statusCode = 404;
+		response.end()
+	}
+}
+
+function getHandler(request,response) {
+	if (request.url === '/products') {
+		response.setHeader('Content-Type', 'application/json');
+		response.statusCode = 200;
+		response.end(JSON.stringify(products));
+	} else if (request.url.split('/')[1] == 'products' && request.url.split('/')[2]) {
+		const product = products.find((f) => f.id == request.url.split('/')[2]);
+
+		if (product) {
+		    response.setHeader('Content-Type', 'application/json');
+			response.statusCode = 200;
+		    response.end(JSON.stringify(product));
+		} else {
+		    response.statusCode = 404;
+		    response.end('Product not found!')
+		}
+	} else {
+	    response.statusCode = 404;
+	    response.end('Not found! Please make sure the route is correct!')
+	}
+}
+
+function postHandler(request,response) {
+	if (request.url == '/products') { 
+        let receivedJSON = '';
+
+		request.on('data', function(data) {
+			receivedJSON += data;
+		})
+
+		request.on('end', function() {
+			if (!receivedJSON) {
+				response.statusCode = 404;
+				response.end('No data! Insert some.');
+				return;
+			}
+
+			const newProduct = JSON.parse(receivedJSON);
+
+            if (!newProduct.name) {
+			    response.statusCode = 400;
+			    response.end('Required field is empty: name');
+			    return;
+			}
+
+			if (!newProduct.description) {
+				response.statusCode = 400;
+				response.end('Required field is empty: description');
+				return;
+			}
+
+
+			if (products.find(f => f.id == newProduct.id)) {
+			    response.statusCode = 400;
+			    response.end('Product already exists!');
+			    return;
+			}
+
+			let products_ids = products.map(el => el.id);
+			newProduct.id = products_ids.length ? Math.max(...products_ids) + 1 : 1;
+
+		    products.push(newProduct);
+		    response.statusCode = 200;
+		    response.setHeader ('Content-Type', 'application/json');
+		    response.end(JSON.stringify(products));
+		});
+	} else {
+		response.statusCode = 404;
+		response.end('Not found! Please make sure the route is correct!');
+	}
+}
+
+function putHandler(request, response) {
+	if (request.url === '/products') {
+		let receivedJSON = '';
+		
+		request.on('data',function(data) {
+			receivedJSON += data;
+		});
+
+		request.on('end',function() {
+			if (!receivedJSON) {
+				response.statusCode = 404;
+				response.end('No data! Insert some.');
+				return;
+			}
+
+			const newArray = JSON.parse(receivedJSON);
+
+			if (newArray.length >= 0) {
+				let count = 1;
+
+				products = newArray.map(product => {
+					return { product, id: count++ }
+				});
+
+				response.setHeader('Content-Type', 'application/json');
+				response.end(JSON.stringify(products));
+			} else {
+				response.statusCode = 400;
+				response.end('The body is invalid!');
+			}
+		});
+	} else if (request.url.split('/')[1] === 'products' && request.url.split('/')[2]) {
+		let receivedJSON ='';
+
+		request.on('data',function(data) {
+			receivedJSON += data;
+		});
+
+		request.on('end',function() {
+			if (!receivedJSON) {
+				response.statusCode = 404;
+				response.end('No data! Insert some.');
+				return;
+			}
+
+			const productBody = JSON.parse(receivedJSON);
+			let id = request.url.split('/')[2];
+
+			if (id) {
+				const product = products.find(f => f.id == id);
+
+				if (!product) {
+					productBody["id"] = id;
+					products.push(productBody);
+					response.statusCode = 200;
+					response.setHeader('Content-Type', 'application/json');
+					response.end(JSON.stringify(products));
+				} else {
+				    product.name = productBody.name;
+				    product.description = productBody.description;
+				    response.statusCode = 200;
+				    response.setHeader('Content-Type', 'application/json');
+				    response.end(JSON.stringify(products));
+				}
+			} else {
+    			response.statusCode = 404;
+    			response.end('ID is missing');
+			}
+		});
+	} else {
+		response.statusCode = 404;
+		response.end('Not found! Please make sure the route is correct.');
+	}
+}
+
+function patchHandler(request, response) {
+	if (request.url.split('/')[1] === 'products' && request.url.split('/')[2]) {
+		let receivedJSON ='';
+
+		request.on('data',function(data) {
+			receivedJSON += data;
+		});
+
+		request.on('end',function() {
+			if (!receivedJSON) {
+				response.statusCode = 404;
+				response.end('No data! Insert some.');
+				return;
+			}
+
+			const productBody = JSON.parse(receivedJSON);
+			let id = request.url.split('/')[2];
+
+			if (id) {
+				const product = products.find(f => f.id == id);
+
+				if (!product) {
+					response.statusCode = 404;
+					response.end('Product not found! Please make sure you are updating an existing product.');
+				} else {
+					if (productBody.name) {
+						product.name = productBody.name;
+					}
+
+					if (productBody.description) {
+						product.description = productBody.description;
+					}
+
+					response.statusCode = 200;
+					response.setHeader('Content-Type', 'application/json');
+					response.end(JSON.stringify(products));
+				}
+			} else {
+				response.statusCode = 404;
+				response.end('ID is missing');
+			}
+		});
+	} else {
+		response.statusCode = 404;
+		response.end('Not found! Please make sure the route is correct.');
+	}
+}
+
+function deleteHandler(request,response) {
+	if(request.url === '/products') {
+		response.setHeader('Content-Type', 'application/json');
+		products = [];
+		response.end(JSON.stringify(products));
+	} else if (request.url.split('/')[1] === 'products' && request.url.split('/')[2]) {
+		const product = products.find(f => f.id == request.url.split('/')[2]);
+		
+        if (product) {
+			products = products.filter(f => f.id !== product.id);
+			response.setHeader('Content-Type', 'application/json');
+			response.end(JSON.stringify(product));
+			response.statusCode = 200;
+		} else {
+			response.statusCode = 404;
+			response.end('Product not found!')
+		}
+	} else {
+		response.statusCode = 404;
+		response.end('Not found! Please make sure the route is correct.')
+	}
+}
+
+function fallbackHandler(response) {
+	response.statusCode = 404;
+	response.end('Method not implemented. Try: HEAD, GET, POST, PUT, PATCH, DELETE');
+}
